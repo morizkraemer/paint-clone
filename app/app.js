360c6323b763b0c79859841bbee6a1c7ae0f7888
morizkraemer <morizkraemer@gmail.com>
2024-02-05 22:23:54 +0100

    implement init canvas function

--- app/app.js
+++ app/app.js
@@ -6,3 +6,17 @@ function el(css) {
 function create(html) {
   return document.createElement(html);
 }
+// SECTION: variables
+const canvasEL = el("#canvas");
+const ctx = canvasEL.getContext("2d");
+canvasEL.style.width = "80%";
+canvasEL.style.height = "80%";
+
+function initCanvas() {
+  ctx.strokeStyle = "black"; // Outline color
+  ctx.lineWidth = 3; // Outline width
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvasEL.width, canvasEL.height);
+  ctx.strokeRect(0, 0, canvasEL.width, canvasEL.height);
+}
+initCanvas();
